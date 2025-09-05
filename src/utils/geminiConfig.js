05cbc40166e3a5ba05bd0d4c7e675846bc0dca9b
fr3d98 <37631735+fr3d98@users.noreply.github.com>
2025-09-05 21:25:04 +0200

    setting up api key

--- src/utils/geminiConfig.js
+++ src/utils/geminiConfig.js
@@ -1,15 +1,9 @@
 import { GoogleGenAI } from "@google/genai";
 import {secret} from '@aws-amplify/backend';
 
-const API_KEY = secret('GEMINI_API_KEY');
+//const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
-console.log('API Key loaded:', API_KEY ? 'Yes' : 'No'); // Debug
-
-if (!API_KEY) {
-  throw new Error("GEMINI_API_KEY non è definita. Controlla il file .env");
-}
-
-const genAI = new GoogleGenAI({ apiKey: API_KEY });
+const genAI = new GoogleGenAI({ apiKey: secret('VITE_GEMINI_API_KEY') });
 
 export const correctText = async (text) => {
   try {
